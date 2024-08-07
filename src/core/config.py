ec026162be4f9e90f84a7835a2d1dff2bb0a844e
Chenger1 <vladislav.marahovskiy@gmail.com>
2024-08-07 21:51:24 +0300

    Init database and alembic

---
+++ src/core/config.py
@@ -0,0 +1,29 @@
+import os
+
+from dotenv import load_dotenv
+from pydantic import BaseConfig
+
+
+load_dotenv()
+
+
+class Config(BaseConfig):
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: str = os.getenv("DB_PORT", "3306")
+
+    DATABASE_URI_FORMAT: str = "postgresql+asyncpg://{user}:{password}@{host}:{port}/{database}".format(
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        database=DB_NAME,
+        port=DB_PORT,
+    )
+
+    class Config:
+        case_sensitive = True
+
+
+config = Config()
---
+++ src/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column
+)
+from sqlalchemy import (
+    String,
+)
+
+from src.db.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(255))
+    name: Mapped[str] = mapped_column(String(255))
+    password: Mapped[str] = mapped_column(String(255))
